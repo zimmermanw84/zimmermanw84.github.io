f7acc41a3e82a7c48129e57ab53250701df8dd13
Walt Zimmerman <zimmermanw84@gmail.com>
2014-10-27 21:22:53 -0700

    added rng.rb

---
+++ rng.rb
@@ -0,0 +1,49 @@
+puts "Pick a number between 1 and 10"
+input = gets.chomp.to_i 
+
+x = (1..10).to_a.sample
+
+
+while input != x
+
+	if input > 10 
+		puts "I said between 1 and 10 dummy"
+	elsif input > x 
+		puts "Wrong answer... To heavy!"
+	elsif input < x 
+		puts "Close.. But no cigar.. Bring it up a bit"
+	end
+input = gets.chomp.to_i
+end
+
+puts "You got it the answer is #{input} and you win nothing!"
+
+puts "Now pick between 11 and 20"
+input2 = gets.chomp.to_i
+
+y = (11..20).to_a.sample
+
+while y != input2
+
+	if input2 > 20 
+		puts "Come on.. I said between 11 and 20"
+	elsif input2 <= 10
+		puts "Really? I said between 11 and 20"	
+	elsif input2 > y 
+		puts "To much.. come down a bit"
+	elsif input2 < y
+		puts "Not enough.. come up a bit"
+	end
+input2 = gets.chomp.to_i
+end
+
+puts "Awesome you got both... Now what is the sum of both numbers?"
+
+input3 = gets.chomp.to_i
+
+while input3 != input + input2
+		puts "Come on you can't add? Try again.."
+input3 = gets.chomp.to_i		
+end
+
+puts "Congrats you can add #{input} and #{input2} together.. You're so freaking smart.. >.>"
